0723d8f6928d36cbb91fd42b85346d1623b4fabf
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2025-02-28 23:53:34 +0700

    add pet view

---
+++ PetManagement/Controllers/PetController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetManagement.Data;
+using PetManagement.Models;
+
+namespace PetManagement.Controllers
+{
+    public class PetController : Controller
+    {
+        private readonly AppDbContext _context;
+        public PetController(AppDbContext context) => _context = context;
+
+        public IActionResult Index()
+        {
+            var pets = _context.Pets.ToList();
+            return View(pets);
+        }
+
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Pet pet)
+        {
+            _context.Pets.Add(pet);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- PetManagement/Program.cs
+++ PetManagement/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +29,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 app.Run();
